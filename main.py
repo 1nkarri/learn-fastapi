e4e38152065501fe10851d95594ac0d8a794250b
inkarri <jhanpieremontes@gmail.com>
2023-02-04 13:11:08 -0500

    Se agrego la peticion de la lista movies en /movies

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ app = FastAPI()
 app.title = "First FastAPI Project"
 app.version = "0.0.1"
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    }
+]
+
 
 @app.get('/', tags=['home'])
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get('/html', tags=['html'])
 async def printhtml():
     return HTMLResponse('<h1>Hello World</h1>')
+
+
+@app.get('/movies', tags=['movies'])
+async def get_movies():
+    return movies
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
